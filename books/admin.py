0e268ee62f1319e835708aa8abc57ceac69f4648
WanP01 <wanpwc@163.com>
2022-08-29 16:03:11 +0800

    bookmanage-admin settting

---
+++ books/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from books.models import BookInfo,PeopleInfo
+
+admin.site.register(BookInfo)
+admin.site.register(PeopleInfo)
+
+
