296b3ee40077bada21133e0e46486d1275369ef5
WanP01 <wanpwc@163.com>
2022-08-29 17:17:20 +0800

    子应用的urls转发

--- bookmanager/urls.py
+++ bookmanager/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+# from books import views
+from books import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('',include('books.urls')),
+    # path('index/',views.index),
+    path('index/',views.index),
 ]
--- books/models.py
+++ books/models.py
@@ -19,3 +19,4 @@ class PeopleInfo(models.Model):
     def __str__(self):
         return self.name
 
+
